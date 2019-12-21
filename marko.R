45f2ac1b127e8438c567da9f2418bf462240ce6c
sasha <bemotic@gmail.com>
2019-12-21 07:36:31 +0200

    comment

---
+++ marko.R
@@ -0,0 +1,16 @@
+library(gdata)                   # load gdata package 
+mydata = read.xls("/home/sasha/ECONOMICS/IAE/OlgaKozak/7.xls")  # read from first sheet
+library(micEcon)
+library(micEconAids)
+priceNames <- c( "p_bread","p_meat","p_fish","p_milk_cheese", "p_butter", "p_oil", "p_sugar")
+shareNames <- c( "w_bread","w_meat","w_fish","w_milk_cheese", "w_butter", "w_oil", "w_sugar")
+## LA-AIDS
+estResult <- aidsEst( priceNames, shareNames, "xfood", data = mydata)
+print( estResult )
+elas( estResult )
+summary(estResult )
+lrtest(estResult,estResult)
+checkConsist( estResult )
+
+
+
